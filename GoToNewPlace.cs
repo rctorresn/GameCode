f6ffc10e8b234b493e760bf872ef34f4dc0dbb8b
rctorresn <36409487+rctorresn@users.noreply.github.com>
2021-04-23 08:09:11 -0500

    Add files via upload

---
+++ GoToNewPlace.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToNewPlace : MonoBehaviour
+{
+
+    public bool needsClick = false;
+    public string uuid;
+    public string newPlaceName = "New Scene Name Here";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Teleport(collision.gameObject.name);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Teleport(collision.gameObject.name);
+    }
+
+    private void Teleport(string objName)
+    { 
+        if (objName == "Player")
+        {
+            if (!needsClick || (needsClick && Input.GetMouseButtonDown(0)))
+            {
+                FindObjectOfType<PlayerController>().nextUuid = uuid;
+                SceneManager.LoadScene(newPlaceName);
+            }
+        }
+    }
+
+}
---
+++ WeaponDamage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponDamage : MonoBehaviour
+{
+    [Tooltip("Cantidad de daño que hará la espada")]
+    public int damage;
+
+    public GameObject bloodAnim;
+    private GameObject hitPoint;
+
+    private void Start()
+    {
+        hitPoint = transform.Find("HitPoint").gameObject;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Enemy"))
+        {
+            if(bloodAnim != null && hitPoint != null) { 
+                Instantiate(bloodAnim, hitPoint.transform.position, hitPoint.transform.rotation);
+            }
+            collision.gameObject.GetComponent<HealthManager>().DamageCharacter(damage);
+        }
+    }
+
+}
